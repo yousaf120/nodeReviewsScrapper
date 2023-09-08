506ef5ffddbbdc1ee19e9b63ee5a220a8752d2b5
Bilal <bilal@gmail.com>
2023-09-08 16:09:22 +0500

    updated code

--- Controllers/reviewScrapper.js
+++ Controllers/reviewScrapper.js
@@ -4,54 +4,72 @@ const cors = require("cors");
 const puppeteer = require("puppeteer"); 
 signuprouter.options("/scrapping", cors());
 signuprouter.post("/scrapping", async(req, res) => {
-		
    (async () => {
     const req_data=req.body
     const URL = "https://www.google.com/maps/search/"+req_data.url;
-    console.log(URL)
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     // Go to your site
     await page.goto(URL);
     await page.setViewport({width: 1580, height: 1424});
+    await page.waitForNavigation();
+    if (page.url().includes("search")) {
+      const reviewsButton = await page.waitForSelector('.Nv2PK');
+      await reviewsButton.click();
+      
+    }
+    try{
     await page.locator('button[data-tab-index="1"]').click();
-   await page.locator('button[data-value="Sort"]').click()
-   await page.locator('xpath=//div[@data-index="1"]').click()
-   const maxWaitTime = 3000; // Maximum time to wait in milliseconds
+    }catch(error){
+      await browser.close();
+      res.send("There are no reviews available")
+      return; 
+    }
+    await page.locator('button[data-value="Sort"]').click()
+    await page.locator('xpath=//div[@data-index="1"]').click()
+   const maxWaitTime = 3000; 
    const startTime = Date.now();
-  
+   await page.waitForTimeout(5000)
+  await page.evaluate(() => {
+    const buttons = document.querySelectorAll('[aria-label="See more"]');
+    buttons.forEach(button => button.click());
+  })
     const data = await page.evaluate(async (maxWaitTime, startTime) => {
-      const results = [];
-    const collectedReviews = new Set();
+      const results = new Set();
       while (true) {
         const items = document.querySelectorAll(".jftiEf");
         
         items.forEach(item => {
-          itemText=item.innerText.split(',')
-          if (!collectedReviews.has(itemText)) {
-            collectedReviews.add(itemText);
-            results.push(itemText);
-          }
+          itemText=item.innerText
+            results.add(itemText);
         });
   
-        // Check if the maximum wait time has been reached
         if (Date.now() - startTime >= maxWaitTime) {
-          break; // Break the loop if the time limit is exceeded
+          break; 
         }
         
   
         await new Promise(resolve => setTimeout(resolve, 1000));
       }
   
-      return results;
-    }, maxWaitTime, startTime); // Pass maxWaitTime and startTime as arguments
+      return [...results];
+    }, maxWaitTime, startTime); 
+
+    const newData = [];
+
+    data.forEach(item => {
+      const parts = item.split(',');
+      newData.push(parts);
+    });
+    
     const newReviews=[]
-    for(let i=0;i<data.length;i++)
+    
+    for(let i=0;i<newData.length;i++)
     {
-      if (data[i][0].includes('NEW')) {
-        itemText=data[i][0].split(',')
-        newReviews.push(itemText)
+      if (newData[i][0].includes('NEW')) {
+        const parts=newData[i][0].split(',')
+        newReviews.push(parts)
       } 
     }
     
